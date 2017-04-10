7293f12a809910be52ab07d38b0c7f1385f94e4e
Ian Byun <ianbyh@gmail.com>
2017-04-10 15:21:28 +1200

    homepage progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,30 @@
 @section('content')
 
 	<img src="images/homemain.jpg" id="home-main">
+	<div class="home1">
+		<h2><p>THE BEST WAY TO SELL IS BY WORD OF MOUTH.</p>
+		<p>SOCIAL MEDIA IS A PLATFORM TO CONNECT.</p>
+		<p>PEOPLE TRUST PEOPLE.</p></h2>
+	</div>
+	<div class="home2">
+		<div class="container-fluid">
+			<div class="col-xs-12">
+				<h2>SOCIAL MATRIX WILL PROVIDE THE MECHANISMS TO MOVE YOUR SOCIAL CONVERSATIONS TO CONVERSIONS</h2>
+			</div>
+			<div class="col-xs-18">
+				<p>Social Matrix offers a platform from which to reach, connect, engage, communicate and sell. We specialise in, and are committed to, partnering our clients to engage with their defined audiences through social media.</p>
+				<p>Through specific interventions and growth, we connect you with your audience through various online platforms which your customers utilize every day.</p>
+				<p>By engaging your stakeholders – not just your ad agency and marketing teams – we look to create a culture where everyone feels comfortable contributing to content. Social media is the new way of marketing.</p>
+				<button type="button" class="btn btn-default">MEET OUR TEAM</button>
+			</div>
+		</div>
+	</div>
+	<div class="home3">
+		<img src="images/homeservices.jpg" id="home-services">
+		<h1>OUR SERVICES</h1>
+	</div>
+	<div class="home4">
+		
+	</div>
 
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,24 +4,42 @@
 	<meta charset="UTF-8">
 	<title>@yield('title')</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="/bootstrap/style.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
 
 </head>
 <body>
 
-	<nav class="navbar navbar-default">
-		<a class="navbar-brand" href="#">{{-- <img src="/images/smlogo.png"> --}}</a>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<a class="navbar-brand" href="#"><img src="/images/smlogo.png"></a>
 		<ul class="nav navbar-nav navbar-right">
-			<li><a href="{{ url('/home') }}">Home</a></li>
-			<li><a href="{{ url('/team') }}">Team</a></li>
-			<li><a href="{{ url('/services') }}">Services</a></li>
-			<li><a href="{{ url('/prices') }}">Prices</a></li>
-			<li><a href="{{ url('/login') }}">Login</a></li>
-			<li><a href="{{ url('/register') }}">Register</a></li>
+			<li><a href="{{ url('/home') }}">HOME</a></li>
+			<li><a href="{{ url('/team') }}">TEAM</a></li>
+			<li><a href="{{ url('/services') }}">SERVICES</a></li>
+			<li><a href="{{ url('/prices') }}">PRICES</a></li>
+			<li><a href="{{ url('/login') }}">LOGIN</a></li>
+			<li><a href="{{ url('/register') }}">REGISTER</a></li>
 		</ul>
 	</nav>
 
 	@yield('content')
+
+	<footer>
+		<div class="footer-box">
+			<div>
+				<p>GET IN TOUCH</p>
+				<P></P>
+			</div>
+			<div>
+				<a href=""><img src="images/facebook.png"></a>
+				<a href=""><img src="images/instagram.png"></a>
+				<a href=""><img src="images/tumblr.png"></a>
+				<a href=""><img src="images/youtube.png"></a>
+			</div>
+			<div>
+				<p>&copy; 2017 SOCIAL MATRIX ALL RIGHTS RESERVED</p>
+			</div>
+		</div>
+	</footer>
 
 </body>
 </html>
